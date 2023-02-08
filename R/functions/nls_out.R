9d80214185a3ad4459819f916bd060a7a8e9e67f
Max Lindmark <max.lindmark@slu.se>
2023-02-08 13:56:01 +0100

    add extracting linf and s.e. from nls_out()

--- R/functions/nls_out.R
+++ R/functions/nls_out.R
@@ -1,12 +1,21 @@
 nls_out <- function(nls_obj) { 
   if(is.null(nls_obj)) { 
     k<-NA 
+    k_se<-NA 
+    linf<-NA
+    linf_se<-NA 
   } else { 
     if(class(nls_obj)!="try-error") {
-      k<-summary(nls_obj)$coefficients[2,1] 
+      k<-summary(nls_obj)$coefficients[2,1]
+      k_se<-summary(nls_obj)$coefficients[2,2] 
+      linf<-summary(nls_obj)$coefficients[1,1]
+      linf_se<-summary(nls_obj)$coefficients[1,2]
     } else {
       k<-NA
+      k_se<-NA
+      linf<-NA
+      linf_se<-NA
     }
   } 
-  return(k)
+  return(data.frame(k, k_se, linf, linf_se))
 }
